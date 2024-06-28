7d863dbb1f733d27fcb30e621ea7ca31597852fd
chetan kaushik <Chetan.Kaushik1@gds.ey.com>
2024-06-28 06:55:32 +0000

    Employee Repo Added

---
+++ EFTrainingLibrary/Models/TrainingsException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTrainingLibrary.Models
+{
+    public class TrainingsException:Exception
+    {
+        public TrainingsException(string msg): base(msg) { }
+       
+    }
+}
