463e69fe84b1383408869e1e8038502251a89a84
Swathy Subramanian <swathy.subramanian@gds.ey.com>
2024-06-28 09:46:28 +0000

    created two new files

---
+++ EFTrainingLibrary/Repos/IEFTrainingRepoAsync.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFTrainingLibrary.Models;
+
+namespace EFTrainingLibrary.Repos
+{
+    internal interface IEFTrainingRepoAsync
+    {
+        Task<List<Training>> GetAllTrainingsAsync();
+        Task<Training> GetTrainingByIdAsync(int trainingId);
+
+        Task<List<Training>> GetTrainingByTechnologyIdAsync(int techId);
+        Task<List<Training>> GetTrainingByTrainerIdAsync(int trainerId);
+
+
+        Task InsertTraining(Training training);
+        Task UpdateTrainingAsync(int trainingId, Training training);
+        Task DeleteTrainingAsync(int trainingId);
+    }
+}
