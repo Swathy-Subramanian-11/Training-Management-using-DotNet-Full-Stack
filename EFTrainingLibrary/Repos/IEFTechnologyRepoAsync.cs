dc58d5b5f7b21d1f6c2a703eb59adc47b373a9f7
Swathy Subramanian <swathy.subramanian@gds.ey.com>
2024-06-28 19:18:15 +0000

    updated

---
+++ EFTrainingLibrary/Repos/IEFTechnologyRepoAsync.cs
@@ -0,0 +1,21 @@
+﻿using EFTrainingLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTrainingLibrary.Repos
+{
+    public interface IEFTechnologyRepoAsync
+    {
+
+        Task<List<Technology>> GetAllTechnologyAsync();
+        Task<List<Technology>> GetTechnologyByType(string type);
+        Task<Technology> GetTechnologyAsync(int techId);
+        Task AddTechnologyAsync(Technology tech);
+        Task UpdateTechnologyAsync(int techId, Technology tech);
+        Task DeleteTechnologyAsync(int techId);
+
+    }
+}
